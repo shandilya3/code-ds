f6341b360b7d37e423aad61b17c79e078ac9fc79
myasslick <myasslick808@gmail.com>
2024-09-25 20:16:12 +0800

    Delete database directory

--- database/_177.sql
+++
@@ -1,9 +0,0 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-BEGIN
-declare m int;
-set m=N-1;
-  RETURN (
-      # Write your MySQL query statement below.
-    select distinct Salary from Employee order by Salary desc limit m, 1  
-  );
-END
--- database/_184.sql
+++
@@ -1,4 +0,0 @@
-select D.Name as Department, E.Name as Employee, E.Salary from Employee E, Department D
-where E.DepartmentId = D.Id
-and (DepartmentId, Salary) in 
-(select DepartmentId, max(Salary) as max from Employee group by DepartmentId)
